6872a415a1302d3043723b4c5032db263fc0429f
shagspb <shagspb@gmail.com>
2017-06-16 21:54:51 +0300

    init

---
+++ animatedimagesviewer/src/main/java/com/helloanatoly/animatedimagesviewer/AnimationUtils.java
@@ -0,0 +1,40 @@
+package com.helloanatoly.animatedimagesviewer;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.view.View;
+import android.view.ViewConfiguration;
+
+/**
+ * Created by shagspb on 15.06.17.
+ * shagspb@gmail.com
+ */
+
+final class AnimationUtils {
+    private AnimationUtils() {
+        throw new AssertionError();
+    }
+
+    static void animateVisibility(final View view) {
+        final boolean isVisible = view.getVisibility() == View.VISIBLE;
+        float from = isVisible ? 1.0f : 0.0f,
+                to = isVisible ? 0.0f : 1.0f;
+
+        ObjectAnimator animation = ObjectAnimator.ofFloat(view, "alpha", from, to);
+        animation.setDuration(ViewConfiguration.getDoubleTapTimeout());
+
+        if (isVisible) {
+            animation.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    view.setVisibility(View.GONE);
+                }
+            });
+        } else view.setVisibility(View.VISIBLE);
+
+        animation.start();
+    }
+
+}
+
---
+++ animatedimagesviewer/src/main/java/com/helloanatoly/animatedimagesviewer/OnDismissListener.java
@@ -0,0 +1,11 @@
+package com.helloanatoly.animatedimagesviewer;
+
+/**
+ * Created by shagspb on 14.06.17.
+ * shagspb@gmail.com
+ */
+
+interface OnDismissListener {
+
+    void onDismiss();
+}
